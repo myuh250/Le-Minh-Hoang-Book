964e06296666875e2213c8b7f8261deb9df75dcc
myuh250 <89782377+myuh250@users.noreply.github.com>
2023-01-03 23:33:00 +0700

    Update 2-1-Generation.cpp

--- 2-1-Generation.cpp
+++ 2-1-Generation.cpp
@@ -2,8 +2,8 @@
 // Le Minh Hoang book pg 6
 
 #include <iostream>
-#include <fstream>
 #include <cstring>
+
 using namespace std;
 
 #define MAX 30
@@ -12,18 +12,23 @@ int main() {
   int x[MAX], n, i;
 
   cin >> n;
-
-  memset(x, 0, sizeof(x)); // Set x=00..0
+  
+  // Set x=00..0
+  memset(x, 0, sizeof(x)); 
   do {
-    for (i = 1; i <= n; i++) cout << x[i]; // Print the current configuration
+    // Print the current configuration
+    for (i = 1; i <= n; i++) cout << x[i]; 
     cout << endl;
 
     i = n; // x[i] is the last element of the array, decrement i until a 0 is encountered or i = 0
     while (i > 0 && x[i] == 1) i--;
-
-    if (i > 0) { // Haven't encountered configuration 11..1
-      x[i] = 1; // Set x[i] to 1
-      memset(x + i + 1, 0, (n - 1) * sizeof(x[0])); // Set x[i+1] = x[i+2] = ... = x[n] := 0
+  
+    // Haven't encountered configuration 11..1
+    if (i > 0) { 
+      // Set x[i] to 1
+      x[i] = 1; 
+      // Set x[i+1] = x[i+2] = ... = x[n] := 0
+      memset(x + i + 1, 0, (n - 1) * sizeof(x[0])); 
     }
   } while (i > 0); // Haven't run out of configurations
 }
